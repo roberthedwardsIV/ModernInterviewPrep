e5137705a1ca846dfdab97a6af3328badf8ad984
roberthedwardsIV <125324864+roberthedwardsIV@users.noreply.github.com>
2025-09-01 18:25:48 -0400

    Sign-In Page Attempt

--- src/app/sign-in/page.jsx
+++ src/app/sign-in/page.jsx
@@ -5,11 +5,13 @@ import {
 import { SignIn } from "@/src/components/SignIn.jsx";
 import { getFirestore } from "firebase/firestore";
 
-export default async function Home() {
+export default async function signIn() {
   
   return (
-    <main className="main__sign-in">
-      <h2>Sign-In Page Added Successfully</h2>
+    <main className="main__signIn">
+        <div>
+            <h2>Sign-In Page Added Successfully</h2>
+        </div>
     </main>
   );
 }
