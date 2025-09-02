c6cd09e1694dc3578ea12460c4b060d116bd6e87
roberthedwardsIV <125324864+roberthedwardsIV@users.noreply.github.com>
2025-09-02 14:15:12 -0400

    Trying again

--- src/app/sign-in/page.jsx
+++ src/app/sign-in/page.jsx
@@ -1,10 +1,11 @@
+// only import needed is for the sign in component
 import SignIn from "@/src/components/Signin.jsx";
 
 
 export default async function Home() {
   
   return (
-    <main className="main__signIn">
+    <main className="main__signin">
         <div>
             <h2>Sign-In Page Added Successfully</h2>
             <SignIn />
---
+++ src/components/SignIn.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+// This components shows one individual restaurant
+// It receives data from src/app/restaurant/[id]/page.jsx
+
+import { React } from "react";
+
+
+export default function SignIn() {
+    return (
+        <>
+            <h2>SignIn Page loaded from JSX file</h2>
+            
+        </>
+    ); 
+}
